6b53c6547ebd724b9ff3adb1607d045438183214
Vishal Kuo <vishalkuo@hotmail.com>
2017-11-14 19:57:18 -0500

    first commit

---
+++ lib/e_poller.ex
@@ -0,0 +1,18 @@
+defmodule EPoller do
+  @moduledoc """
+  Documentation for EPoller.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EPoller.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/e_poller_test.exs
@@ -0,0 +1,8 @@
+defmodule EPollerTest do
+  use ExUnit.Case
+  doctest EPoller
+
+  test "greets the world" do
+    assert EPoller.hello() == :world
+  end
+end
