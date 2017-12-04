69c2bdcdc24fdc30ed0ffd81f3941761039671b2
Vishal Kuo <vishalkuo@hotmail.com>
2017-12-03 23:51:28 -0500

    Cleanup

--- lib/e_poller.ex
+++ lib/e_poller.ex
@@ -1,15 +1,27 @@
 defmodule EPoller do
+  use Agent
+
   @moduledoc """
   Documentation for EPoller.
   """
 
-  @doc """
-  Hello world.
+  @spec start_link(binary, ExAws.SQS.receive_message_opts()) :: {:ok, PID}
+  def start_link(queue_name, config \\ []) do
+    Agent.start_link(fn -> %{:queue_name => queue_name, :config => config} end)
+  end
 
-  ## Examples
+  @spec poll(PID) :: {:ok, map()}
+  def poll(pid) do 
+     Agent.get(pid, fn m -> 
+      {:ok, result} = ExAws.SQS.receive_message(m[:queue_name]) 
+        |> ExAws.request
 
-      iex> EPoller.hello
-      :world
+      mapped_result = result
+        |> Map.get(:body)
+        |> Map.get(:messages)
+        |> Enum.map(fn m -> m[:body] end)
 
-  """
+      {:ok, mapped_result}
+    end)
+  end
 end
