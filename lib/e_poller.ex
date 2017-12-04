38cd7afbc95ca0ed10f6767c0e7f5c72fa838289
Vishal Kuo <vishalkuo@hotmail.com>
2017-12-04 17:47:55 -0500

    Update poller

--- lib/e_poller.ex
+++ lib/e_poller.ex
@@ -11,9 +11,13 @@ defmodule EPoller do
 
   Returns `{:ok, PID}`
   """
-  @spec start_link(binary, ExAws.SQS.receive_message_opts()) :: {:ok, PID}
-  def start_link(queue_name, config \\ []) do
-    Agent.start_link(fn -> %{:queue_name => queue_name, :config => config} end)
+  @spec start_link(binary, function(), ExAws.SQS.receive_message_opts()) :: {:ok, PID}
+  def start_link(queue_name, handler, config \\ []) do
+    Agent.start_link(fn -> 
+      %{:queue_name => queue_name, 
+      :handler => handler,
+      :config => config} 
+    end)
   end
 
   @doc """
@@ -23,16 +27,29 @@ defmodule EPoller do
   """
   @spec poll(PID) :: {:ok, map}
   def poll(pid) do 
-     Agent.get(pid, fn m -> 
-      {:ok, result} = ExAws.SQS.receive_message(m[:queue_name], m[:config]) 
+     Agent.get(pid, fn state -> 
+
+      queue_name = state[:queue_name]
+      {:ok, result} = ExAws.SQS.receive_message(queue_name, state[:config]) 
         |> ExAws.request
 
-      mapped_result = result
+      result
         |> Map.get(:body)
         |> Map.get(:messages)
-        |> Enum.map(fn m -> m[:body] end)
+        |> Enum.map(fn m -> 
+          state[:handler].(m[:body])
+          m
+        end)
+        |> Enum.each(fn m -> 
+          delete_message(queue_name, m) 
+        end)
 
-      {:ok, mapped_result}
+        :ok
     end)
+  end
+
+  defp delete_message(queue_name, body) do 
+    ExAws.SQS.delete_message(queue_name, body[:receipt_handle]) 
+      |> ExAws.request
   end
 end
