6c4cd81c9b48b597a708cbe7b85f5865d08c0657
Vishal Kuo <vishalkuo@hotmail.com>
2017-12-04 22:25:47 -0500

    Work around config

--- lib/e_poller.ex
+++ lib/e_poller.ex
@@ -7,31 +7,41 @@ defmodule EPoller do
   """
 
   @doc """
-  Initializes a queue poller with the given `queue_name` and `config`. 
+  Initializes a queue poller with the given `queue_name``, `message_handler`, and `config`. 
 
   Returns `{:ok, PID}`
   """
-  @spec start_link(binary, function(), ExAws.SQS.receive_message_opts()) :: {:ok, PID}
-  def start_link(queue_name, handler, config \\ []) do
+  @spec start_link(binary, function(), list) :: {:ok, PID}
+  def start_link(
+    queue_name, 
+    handler, 
+    config \\ []) do
+    formatted_conf = EPoller.Config.get_full_config(config)
     Agent.start_link(fn -> 
-      %{:queue_name => queue_name, 
+      %{:queue_name => queue_name,
       :handler => handler,
-      :config => config} 
+      :config => formatted_conf} 
     end)
   end
 
   @doc """
-  Polls the queue with the config for the process assigned to `pid`
+  Polls the queue with the config for the process assigned to `pid`. 
+
+  For each message in the polling result this function: 
+  1. Calls the handler on that message
+  2. Deletes the message from the queue (provided the handler doesn't throw an exception)
 
   Returns `{:ok, map}`
   """
   @spec poll(PID) :: {:ok, map}
   def poll(pid) do 
      Agent.get(pid, fn state -> 
-
       queue_name = state[:queue_name]
-      {:ok, result} = ExAws.SQS.receive_message(queue_name, state[:config]) 
-        |> ExAws.request
+      sqs_conf = state[:config][:sqs_conf]
+      region = state[:config][:region]
+
+      {:ok, result} = ExAws.SQS.receive_message(queue_name, sqs_conf) 
+        |> ExAws.request(region: region)
 
       result
         |> Map.get(:body)
@@ -42,7 +52,7 @@ defmodule EPoller do
         end)
         
         :ok
-    end)
+    end, 30_000)
   end
 
   defp delete_message(queue_name, body) do 
--- lib/e_poller/config.ex
+++ lib/e_poller/config.ex
@@ -1,15 +1,30 @@
 defmodule EPoller.Config do
+  @moduledoc """
+  This module contains the configuration management for queue polling
+  """
+
   @default_config [wait_time_seconds: 20, 
     visibility_timeout: 10, 
-    max_messages: 10]
+    max_messages: 10, 
+    region: "us-east-1"]
 
+    @doc """
+    Gets the user and default config for polling and merges them
+
+    Why have a config abstraction for virtually the same values seen in ExAws.SQS?
+    1. In case config changes from the ExAws side. EPoller's client shouldn't know what the underlying
+    implementation of SQS polling is and shouldn't have to deal with it
+    2. We don't necessarily want to config _all_ parameters. Polling might not function as promised if 
+    we start tweaking too many knobs (relates to 1). 
+    """
     @spec get_full_config(map) :: map
     def get_full_config(user_config) do 
       res = Keyword.merge(@default_config, user_config)
-      [
+      sqs_conf = [
         max_number_of_messages: res[:max_messages],
         visibility_timeout: res[:visibility_timeout],
         wait_time_seconds: res[:wait_time_seconds]
       ]
+      %{:region => res[:region], :sqs_conf => sqs_conf}
     end 
 end
